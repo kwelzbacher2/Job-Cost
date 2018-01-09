92f85d5a399e72c3547eb0878abf6b4fbf875a24
K Welzbacher <35229915+kwelzbacher2@users.noreply.github.com>
2018-01-09 11:37:51 -0500

    Add files via upload
    source files for visual studio project

---
+++ Job.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoJobs
+{
+    class Job
+    {
+        //declare variables
+        private string description;
+        private double time;
+        private double rate;
+        private double total;
+
+        //create constructor
+        public Job(string description, double time, double rate)
+        {
+            Description = description;
+            Time = time;
+            Rate = rate;
+        }
+
+        //create method to overload + operator
+        public static Job operator +(Job j1, Job j2)
+        {
+            return new Job(j1.description + j2.description, j1.time + j2.time, (j1.rate + j2.rate) / 2);
+        }
+
+        //make get and set for properties
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            private set
+            {
+                description = value;
+            }
+        }
+        public double Time
+        {
+            get
+            {
+                return time;
+            }
+            private set
+            {
+                time = value;
+                total = rate * time;
+            }
+        }
+
+        public double Rate
+        {
+            get
+            {
+                return rate;
+            }
+            set
+            {
+                rate = value;
+                total = rate * time;
+            }
+        }
+        public double Total
+        {
+            get
+            {
+                return total;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+/* Ch9 Exercise 10a
+ * Programmer: Katie Welzbacher
+ * Date: 3/22/17
+ */
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DemoJobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create new Job objects
+            Job floor = new Job("wipe floor", 4.8, 8.75);
+            Job paint = new Job("paint trimming", 36.8, 30.75);
+            Job both = floor + paint;
+            //print out each job
+            WriteLine("Information about the first job.");
+            PrintOut(floor);
+            WriteLine("Information about the second job.");
+            PrintOut(paint);
+            WriteLine("Information about the combined jobs.");
+            PrintOut(both);
+            WriteLine("Press the <Enter> key to end this program.");
+            Console.Read();
+
+        }
+        //method to print out job information
+        public static void PrintOut(Job job)
+        {
+            WriteLine();
+            WriteLine("Job Description: " + job.Description);
+            WriteLine("Job Completion Time: " + job.Time + " hours. Job Rate Per Hour: " + job.Rate.ToString("C") + ".");
+            WriteLine("Job Total Cost: " + job.Total.ToString("c") + ".");
+            WriteLine();
+        }
+    }
+}
